7c59c3c0430d269ddce3b8f53018998e7eeaa002
youbyoub48 <ayou8@hotmail.fr>
2022-03-13 19:47:09 +0100

    ajout de la lecture en arrière plan

--- client/telecharger.py
+++ client/telecharger.py
@@ -1,4 +1,4 @@
-import requests, urllib.request, os
+import requests, urllib.request, vlc, time
 
 r = requests.get("http://127.0.0.1:8000/api")
 print(r.status_code)
@@ -8,4 +8,10 @@ for element in dico:
     musique = element
     urllib.request.urlretrieve(dico[musique], f"{musique}.mp3")
 
-os.system(f"{musique}.mp3")
+p = vlc.MediaPlayer(f"{musique}.mp3")
+p.play()
+time.sleep(0.5)
+Millisecondes = p.get_length()
+Secondes = Millisecondes*0.001
+print(Secondes)
+time.sleep(Secondes)
